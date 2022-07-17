d29fa44ee24ffc60427cdf55731a3306e1f94495
Vinicius Mafra <viinymafra@gmail.com>
2022-07-17 10:51:19 -0300

    Start new repository

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { promise } from 'protractor';
+import { LoginPayload } from 'src/app/models/payloads/login.payload';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+
+  constructor(
+    private readonly toastController: ToastController,
+    private readonly alertController: AlertController,
+  ) { }
+
+  public loginPayload: LoginPayload = {
+    email: '',
+    password: '',
+  }
+
+  public isLoading: boolean = false;
+
+  public async login(): Promise<void> {
+    this.isLoading = true;
+
+    // toast
+    const toast = await this.toastController.create({
+      message: 'Logando...',
+      duration: 2000 //2s
+    });
+    toast.present();
+
+    // alert
+    const alert = await this.alertController.create({
+      header: 'Hello World',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => { console.log('Ok!') }
+        }
+      ]
+    });
+    alert.present();
+
+    console.log(this.loginPayload);
+  }
+
+  public canLogin(): boolean {
+    const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+
+    const emailIsValid = regex.test(this.loginPayload.email)
+
+    if (emailIsValid && this.loginPayload.password.length >= 6)
+      return true;
+
+    return false;
+  }
+
+}
