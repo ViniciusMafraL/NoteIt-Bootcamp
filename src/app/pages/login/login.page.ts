885cc246c18cc90bf112aaab99b6282ac145c7d4
Vinicius Mafra <viinymafra@gmail.com>
2022-07-23 09:08:42 -0300

    import pages - register

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { LoginPayload } from 'src/app/models/payloads/login.payload';
 import { HelperService } from 'src/app/services/helper.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginPage {
 
   constructor(
     private readonly helper: HelperService,
+    private readonly router: Router,
   ) { }
 
   public loginPayload: LoginPayload = {
@@ -19,7 +21,17 @@ export class LoginPage {
     password: '',
   }
 
+  public registerPayload = {
+    name: '',
+    email: '',
+    confirmEmail: '',
+    password: '',
+    confirmPassword: '',
+  }
+
   public isLoading: boolean = false;
+
+  public isSigning: boolean = false;
 
   public async login(): Promise<void> {
     if (!this.canLogin())
@@ -31,29 +43,51 @@ export class LoginPage {
     await this.helper.showToast('Carregando...');
 
     // alert
-    await this.helper.showAlert('Salve!', [
+    await this.helper.showAlert('Hello', [
       {
-        text: 'Sim',
-        handler: () => console.log('AEEEEEEEEE!'),
+        text: 'Ok',
+        handler: () => console.log('Ok!'),
       },
       {
-        text: 'Não',
-        handler: () => console.log('Esse n poh!'),
+        text: 'Outro',
+        handler: () => console.log('Outro!'),
       }
     ]);
 
     console.log(this.loginPayload);
+    await this.router.navigate(['/feed']);
   }
 
   public canLogin(): boolean {
     const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
 
-    const emailIsValid = regex.test(this.loginPayload.email)
+    const emailIsValid = regex.test(this.loginPayload.email);
 
     if (emailIsValid && this.loginPayload.password.length >= 6)
       return true;
 
     return false;
+  }
+
+  public canRegister(): boolean {
+    const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+
+    if(this.registerPayload.name.trim().length<=0)
+     return false;
+
+    if(!regex.test(this.registerPayload.email))
+      return false;
+
+    if(this.registerPayload.email !== this.registerPayload.confirmEmail)
+      return false;
+
+    if(this.registerPayload.password.length < 6)
+      return false;
+
+    if(this.registerPayload.password !== this.registerPayload.confirmPassword)
+      return false;
+
+    return true;
   }
 
   public logoClick($event: boolean): void {
